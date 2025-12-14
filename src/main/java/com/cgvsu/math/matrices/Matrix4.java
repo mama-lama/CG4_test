41d3c82cf89f2e671fcf3192edb4a6d18f37861b
MoonBunny70 <dsidelnikov671@gmail.com>
2025-12-15 02:56:44 +0300

    Methods for working with matrices have been added to the Matrix4 class.

--- src/main/java/com/cgvsu/math/matrices/Matrix4.java
+++ src/main/java/com/cgvsu/math/matrices/Matrix4.java
@@ -1,5 +1,7 @@
 package com.cgvsu.math.matrices;
 
+import com.cgvsu.math.vectors.Vector4f;
+
 public class Matrix4 {
     private float[][] data = new float[4][4];
 
@@ -35,6 +37,77 @@ public class Matrix4 {
     // Нулевая
     public static Matrix4 zeroMatrix() {
         return new Matrix4();
+    }
+
+    // --------------------------- РАБОТА С МАТРИЦАМИ --------------------------
+    // Сложение
+    public Matrix4 addition(Matrix4 other) {
+        float[][] result = new float[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result[i][j] = this.data[i][j] + other.data[i][j];
+            }
+        }
+
+        return new Matrix4(result);
+    }
+    // Вычитание
+    public Matrix4 subtraction(Matrix4 other) {
+        float[][] result = new float[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result[i][j] = this.data[i][j] - other.data[i][j];
+            }
+        }
+
+        return new Matrix4(result);
+    }
+    // Умножение на вектор
+    public Vector4f multiplication(Vector4f vector) {
+        float x = data[0][0] * vector.getX() + data[0][1] * vector.getY() +
+                data[0][2] * vector.getZ() + data[0][3] * vector.getW();
+
+        float y = data[1][0] * vector.getX() + data[1][1] * vector.getY() +
+                data[1][2] * vector.getZ() + data[1][3] * vector.getW();
+
+        float z = data[2][0] * vector.getX() + data[2][1] * vector.getY() +
+                data[2][2] * vector.getZ() + data[2][3] * vector.getW();;
+
+        float w = data[3][0] * vector.getX() + data[3][1] * vector.getY() +
+                data[3][2] * vector.getZ() + data[3][3] * vector.getW();;
+
+        return new Vector4f(x, y, z, w);
+    }
+    // Умножение на матрицу
+    public Matrix4 multiplication(Matrix4 other) {
+        float[][] result = new float[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                float sum = 0;
+                for (int k = 0; k < 4; k++) {
+                    sum += this.data[i][k] * other.data[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+
+        return new Matrix4(result);
+    }
+
+    // Транспонирование
+    public Matrix4 transposition() {
+        float[][] result = new float[4][4];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                result[j][i] = this.data[i][j];
+            }
+        }
+
+        return new Matrix4(result);
     }
 
     // ------------------------- ВЫВОД МАТРИЦЫ -----------------------------------
